0bf19254c36c7ff47a7e1122cb4a7c1d081e5d40
Kendall Nakai <kendallnakai@gmail.com>
2020-01-30 10:36:21 -0800

    Uploading to test on Heroku

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,13 @@
 /*
  * GET home page.
  */
+ exports.view = function(req, res){
+  res.render('index', {
+    'name': 'Waiting in Line',
+    'image': 'lorempixel.people.1.jpeg',
+    'id': 'project1'
+  });
+};
 
 exports.view = function(req, res){
   res.render('index');
