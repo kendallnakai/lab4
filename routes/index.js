ef8bea64abfbd69859b4820474393083c324132d
Kendall Nakai <kendallnakai@gmail.com>
2020-01-30 11:01:31 -0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -7,14 +7,7 @@
     'name': 'Waiting in Line',
     'image': 'lorempixel.people.1.jpeg',
     'id': 'project1'
-},
-{ "name": "Needfinding",
-    "image": "lorempixel.city.1.jpeg",
-    "id": "project2"
-},
-...
-]
-});
+  });
 };
 
 exports.view = function(req, res){
